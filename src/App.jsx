c5946f79d7c22c32f0be49af323289a6faf78732
Uday Sahu <udaysahu982@gmail.com>
2025-12-25 19:05:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar></Navbar>
+     <div className=''>hello</div>
+    </>
+  )
+}
+
+export default App
