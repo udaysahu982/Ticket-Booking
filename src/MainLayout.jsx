de0dc37209d828d74a03c17e2138f6f741e888bc
Uday Sahu <udaysahu982@gmail.com>
2026-01-07 23:17:25 +0530

    commit

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 import LocationSearch from './PopWindows/LocationSearch'
 import { Outlet } from 'react-router-dom'
-import Footer from './assets/Footer/footer'
+import Footer from './Footer/Footer'
 
 const MainLayout = () => {
   return (
